5962d6363f182c262994a1bf1094a6f41cfc6117
Austin Sisson <austinsisson@gmail.com>
2014-10-31 22:55:04 +0000

    started sign in test in profiles_spec

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe "Visting profiles" do
   
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  
   
   before do
     @user = authenticated_user
@@ -22,6 +25,25 @@ describe "Visting profiles" do
       expect(page).to have_content(@post.title)
       expect(page).to have_content(@comment.body)
     end
+  end
+  
+  describe "user is signed in" do
     
+    before do
+      login_as(@user, :scope => :user)
+      @post = associated_post(user: @user)
+      @comment = Comment.new(user: @user, post: @post, body: "A comment")
+      allow(@comment).to receive(:send_favorite_emails)
+      @comment.save
+    end
+    
+    it "visits own profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
   end
 end
