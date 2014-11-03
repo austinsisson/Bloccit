b5e6a78e9a1f807a08cdece62e3761fcef2261ed
Austin Sisson <austinsisson@gmail.com>
2014-11-03 01:51:28 +0000

    implemented Factory Girl, refactored tests

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ end
 group :test do
   gem 'rspec-rails', '~> 3.1.0'
   gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
 end
 
 gem 'sass-rails', '~> 4.0.3'
--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -2,17 +2,13 @@ require 'rails_helper'
 
 describe "Visting profiles" do
   
-  include TestFactories
   include Warden::Test::Helpers
   Warden.test_mode!
   
-  
   before do
-    @user = authenticated_user
-    @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, post: @post, body: "A comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save
+    @user = create(:user)
+    @post = create(:post, user: @user)
+    @comment = create(:comment, post: @post, user: @user)
   end
   
   describe "not signed in" do
@@ -31,10 +27,6 @@ describe "Visting profiles" do
     
     before do
       login_as(@user, :scope => :user)
-      @post = associated_post(user: @user)
-      @comment = Comment.new(user: @user, post: @post, body: "A comment")
-      allow(@comment).to receive(:send_favorite_emails)
-      @comment.save
     end
     
     it "visits own profile" do
